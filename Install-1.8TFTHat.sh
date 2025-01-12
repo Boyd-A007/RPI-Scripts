c1602d5099ed6a7f6d4553093598398888e5be61
Alpha <12474626+Boyd-A007@users.noreply.github.com>
2025-01-12 08:04:55 -0800

    Update Install-1.8TFTHat.sh

--- Install-1.8TFTHat.sh
+++ Install-1.8TFTHat.sh
@@ -1,1 +1,13 @@
 
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install -y git python3-venv python3-pip
+cd ~
+python -m venv env --system-site-packages
+source env/bin/activate
+pip3 install --upgrade adafruit-python-shell click
+git clone https://github.com/adafruit/Raspberry-Pi-Installer-Scripts.git
+cd Raspberry-Pi-Installer-Scripts
+sudo -E env git clone https://github.com/adafruit/Raspberry-Pi-Installer-Scripts.git
+cd Raspberry-Pi-Installer-Scripts
+sudo -E env PATH=$PATH python3 adafruit-pitft.py --install-type=console
